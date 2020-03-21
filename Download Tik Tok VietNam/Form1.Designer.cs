725b0000c9b0d0088a028ff236f031a723a57886
mytran1998.github.io <43548781+mytran1998@users.noreply.github.com>
2020-03-21 23:29:32 +0700

    Commit

---
+++ Download Tik Tok VietNam/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Download_Tik_Tok_VietNam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name: ";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(96, 43);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(216, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Image = global::Download_Tik_Tok_VietNam.Properties.Resources.play_green;
+            this.btnStart.Location = new System.Drawing.Point(388, 28);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 48);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 127);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Download Video TikTok ";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Download Tik Tok VietNam/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Download_Tik_Tok_VietNam
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecuteCommand(string name)
+        {
+            string commandText = @"/c tiktok-dl " + name;
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo
+                (Environment.GetFolderPath(Environment.SpecialFolder.System) + "\\" + @"cmd.exe", commandText);
+            //startInfo.WorkingDirectory = Application.StartupPath;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void KillProcess()
+        {
+            Process[] processes = Process.GetProcessesByName("cmd");
+            foreach(Process p in processes)
+            {
+                p.Kill();
+                p.Dispose();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string UserName = txtUserName.Text;
+            if(UserName != null)
+                ExecuteCommand(UserName);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            KillProcess();
+        }
+    }
+}
